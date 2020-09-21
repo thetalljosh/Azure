2288314dc5824922f8ce37d4f982567c22a3c0fb
thetalljosh <49563871+thetalljosh@users.noreply.github.com>
2020-09-21 09:57:34 -0400

    Add files via upload

---
+++ Win10_Setup.ps1
@@ -0,0 +1,32 @@
+﻿configuration Windows_10_STIG
+{
+    param
+    (
+        [parameter()]
+        [string]
+        $NodeName = 'localhost'
+    )
+
+    Import-DscResource -ModuleName "PowerSTIG"
+
+
+
+    Node $NodeName
+    {
+        WindowsClient BaseLine
+        {
+            OsVersion   = '10'
+            #OsRole      = '1'
+            StigVersion = '1.23'
+            OrgSettings = "\\fs\Share02\Automation-Resources\PowerSTIG_MOFs\zOrgSettings\WindowsClient-10-1.23.org.xml"
+            # DomainName  = 'your.domain'
+            # ForestName  = 'your.domin'
+        }
+      
+    
+    }}
+  
+    
+
+
+Windows_10_STIG -OutputPath \\fs\Share02\Automation-Resources\Azure\DSC
